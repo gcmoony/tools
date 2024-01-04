6ee4d1aa329a73246ae9be213ab40305d4761c5a
GCMoony <47188474+GCMoony@users.noreply.github.com>
2024-01-03 19:09:48 -0700

    Updated homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,23 @@
 'use client'
-import Image from 'next/image'
-import TicketQueue from './components/TicketQueue/TicketQueue';
-import ToDoForm from './components/ToDoForm/ToDoForm';
+import Item from './components/Item/Item';
 import PomoTimer from './components/PomoTimer/PomoTimer';
+import RegularForm from './components/RegularForm/RegularForm';
 
 
 export default function Home() {
 
 
   return (
-    <main>
-      <h1 className='text-center font-bold text-3xl'>Welcome!</h1>
-      <h2  className='text-center text-xl mb-10'>Below is your work area</h2>
-      <div className='outline-slate-500 outline-dashed m-3'>
-        {/* Add your component below this line */}
-          <PomoTimer />
-        {/* Add your component above this line */}
+    <main className='text-white p-5 min-h-screen bg-black'>
+      <h1 className='text-4xl font-semibold'><span className='inline-block p-1 bg-black text-white'>Tool</span><span className=' p-1 rounded bg-yellow-400 text-black inline-block'>Hub</span></h1>
+      <h2 className='text-sm mb-8'>Just what you&apos;re looking for</h2>
+      <div className='flex items-center flex-col '>
+        <header className='underline text-xl'>Tools</header>
+        <ul className='flex flex-col'>
+          <li><a className='p-2 block hover:text-yellow-400' href='/tools/Pomo'>Pomodoro Timer</a></li>
+          <li><a className='p-2 block hover:text-yellow-400' href='/tools/PasswordGenerator'>Password Generator</a></li>
+          {/* <li><a className='p-2 block hover:text-yellow-400' href='#'>To Do List</a></li> */}
+        </ul>
       </div>
     </main>
   );
