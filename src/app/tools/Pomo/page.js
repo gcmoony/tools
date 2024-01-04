5e2b5b29896ef9a00324f2c3ba2953667b2ec63a
GCMoony <47188474+GCMoony@users.noreply.github.com>
2024-01-03 19:11:14 -0700

    Moved file

---
+++ src/app/tools/Pomo/page.js
@@ -0,0 +1,14 @@
+'use client'
+import PomoTimer from "@/app/components/PomoTimer/PomoTimer";
+
+export default function Pomo() {
+
+
+  return (
+    <main className=" mt-8 min-h-screen">
+        {/* Add your component below this line */}
+        <PomoTimer />
+        {/* Add your component above this line */}
+    </main>
+  );
+}
