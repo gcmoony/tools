40510605f45bc761195d75690970463d798a91d3
gcmoony <47188474+gcmoony@users.noreply.github.com>
2025-01-27 01:48:11 +0900

    Adjusted styling

--- src/app/tools/ToDoList/page.jsx
+++ src/app/tools/ToDoList/page.jsx
@@ -3,7 +3,8 @@ import ToDoForm from "@/app/components/ToDoForm/ToDoForm"
 
 export default function ToDoList() {
   return (
-    <main className=' mt-8 min-h-screen'>
+    <main className=' mt-8 min-h-screen max-w-screen-xl mx-auto'>
+      <h2 className='text-center text-5xl pb-8'>Your To-Do List</h2>
       {/* Add your component below this line */}
       <ToDoForm />
       {/* Add your component above this line */}
